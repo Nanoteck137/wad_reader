91475cfaf603e250a94cbf420980a0f6b9ccfd43
Patrik M. Rosenström <patrik.millvik@gmail.com>
2022-07-11 15:51:52 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ mod wad;
 mod util;
 
 /// TODO(patrik):
-///   - Parse texture data
 ///   - Map format
 ///     - Textures
 
@@ -102,7 +101,9 @@ fn generate_sector_floor(map: &wad::Map, sector: &wad::Sector) -> mime::Mesh {
     mime::Mesh::new(mesh.vertex_buffer, mesh.index_buffer)
 }
 
-fn generate_sector_ceiling(map: &wad::Map, sector: &wad::Sector) -> mime::Mesh {
+fn generate_sector_ceiling(map: &wad::Map, sector: &wad::Sector)
+    -> mime::Mesh
+{
     let mut mesh = Mesh::new();
 
     let mut index = 0;
@@ -115,7 +116,10 @@ fn generate_sector_ceiling(map: &wad::Map, sector: &wad::Sector) -> mime::Mesh {
             let start = map.vertex(segment.start_vertex);
 
             let color = COLOR_TABLE[index];
-            vertices.push(mime::Vertex::new(start.x, sector.ceiling_height, start.y, color));
+            vertices.push(mime::Vertex::new(start.x,
+                                            sector.ceiling_height,
+                                            start.y,
+                                            color));
         }
 
         index += 1;
@@ -217,148 +221,7 @@ fn generate_sector_wall(map: &wad::Map, sector: &wad::Sector) -> mime::Mesh {
     mime::Mesh::new(mesh.vertex_buffer, mesh.index_buffer)
 }
 
-/*
-fn generate_sector_floor_ceiling(map: &wad::Map, sector: &wad::Sector)
-    -> mime::Mesh
-{
-    let mut index = 0;
-
-    for sub_sector in &sector.sub_sectors {
-        let mut floor = Vec::new();
-        let mut ceiling = Vec::new();
-
-        for segment in 0..sub_sector.count {
-            let segment = map.segments[sub_sector.start + segment];
-            let start = map.vertex(segment.start_vertex);
-
-            let color = COLOR_TABLE[index];
-            floor.push(mime::Vertex::new(start.x, sector.floor_height, start.y, color));
-            ceiling.push(mime::Vertex::new(start.x, sector.ceiling_height, start.y, color));
-        }
-
-        index += 1;
-        if index >= COLOR_TABLE.len() {
-            index = 0;
-        }
-
-        mesh.add_vertices(floor, true, true);
-        mesh.add_vertices(ceiling, false, true);
-    }
-}
-*/
-
-/*
-fn generate_sector_walls(map: &wad::Map, sector: &wad::Sector,
-                         mesh: &mut Mesh)
-{
-    let mut index = 0;
-    for sub_sector in &sector.sub_sectors {
-        for segment in 0..sub_sector.count {
-            let segment = map.segments[sub_sector.start + segment];
-
-            if segment.linedef != 0xffff {
-                let mut wall = Vec::new();
-                let linedef = map.linedefs[segment.linedef];
-                let line = linedef.line;
-                let start = map.vertex(line.start_vertex);
-                let end = map.vertex(line.end_vertex);
-
-                let color = COLOR_TABLE[index];
-
-                if linedef.flags & wad::LINEDEF_FLAG_IMPASSABLE == wad::LINEDEF_FLAG_IMPASSABLE &&
-                    linedef.flags & wad::LINEDEF_FLAG_TWO_SIDED != wad::LINEDEF_FLAG_TWO_SIDED
-                {
-                    wall.push(mime::Vertex::new(start.x, sector.floor_height, start.y, color));
-                    wall.push(mime::Vertex::new(end.x, sector.floor_height, end.y, color));
-                    wall.push(mime::Vertex::new(end.x, sector.ceiling_height, end.y, color));
-                    wall.push(mime::Vertex::new(start.x, sector.ceiling_height, start.y, color));
-                }
-
-                mesh.add_vertices(wall, false, false);
-            }
-        }
-
-        index += 1;
-        if index >= COLOR_TABLE.len() {
-            index = 0;
-        }
-    }
-}
-*/
-
-/*
-fn generate_sector_extra(map: &wad::Map, sector: &wad::Sector,
-                         mesh: &mut Mesh)
-{
-    let mut index = 0;
-
-    for sub_sector in &sector.sub_sectors {
-        for segment in 0..sub_sector.count {
-            let segment = map.segments[sub_sector.start + segment];
-
-            if segment.linedef != 0xffff {
-                let linedef = map.linedefs[segment.linedef];
-                let line = linedef.line;
-
-                let start = map.vertex(line.start_vertex);
-                let end = map.vertex(line.end_vertex);
-
-                let mut generate_wall = |front, back, clockwise| {
-                    let mut verts = Vec::new();
-
-                    let color = COLOR_TABLE[index];
-                    verts.push(
-                        mime::Vertex::new(start.x, front, start.y, color));
-                    verts.push(
-                        mime::Vertex::new(end.x, front, end.y, color));
-                    verts.push(
-                        mime::Vertex::new(end.x, back, end.y, color));
-                    verts.push(
-                        mime::Vertex::new(start.x, back, start.y, color));
-
-                    mesh.add_vertices(verts, clockwise, false);
-
-                    index += 1;
-                    if index >= COLOR_TABLE.len() {
-                        index = 0;
-                    }
-                };
-
-                if linedef.front_sidedef.is_some() &&
-                    linedef.back_sidedef.is_some()
-                {
-                    let front_sidedef = linedef.front_sidedef.unwrap();
-                    let front_sidedef = map.sidedefs[front_sidedef];
-
-                    let back_sidedef = linedef.back_sidedef.unwrap();
-                    let back_sidedef = map.sidedefs[back_sidedef];
-
-                    let front_sector = &map.sectors[front_sidedef.sector];
-                    let back_sector = &map.sectors[back_sidedef.sector];
-
-                    // Generate the floor difference
-                    if front_sector.floor_height != back_sector.floor_height {
-                        let front = front_sector.floor_height;
-                        let back = back_sector.floor_height;
-
-                        generate_wall(front, back, false);
-                    }
-
-                    // Generate the height difference
-                    if front_sector.ceiling_height != back_sector.ceiling_height {
-                        let front = front_sector.ceiling_height;
-                        let back = back_sector.ceiling_height;
-                        generate_wall(front, back, true);
-                    }
-                }
-            }
-        }
-    }
-}
-*/
-
-fn generate_sector_from_wad(map: &wad::Map,
-                            sector: &wad::Sector)
+fn generate_sector_from_wad(map: &wad::Map, sector: &wad::Sector)
     -> mime::Sector
 {
     let floor_mesh = generate_sector_floor(map, sector);
